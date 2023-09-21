60b4faa5b1b0c4ea3e20a72946b49fa671bb1814
madi0002 <adityamudili99@gmail.com>
2023-09-21 12:50:57 +0530

    Unit Testing

--- app/src/test/java/com/example/b3sync/ExampleUnitTest.kt
+++ app/src/test/java/com/example/b3sync/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.b3sync
 
+import com.example.b3sync.data.Dice
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -13,5 +14,10 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+    fun DiceTest(){
+        val dice = Dice(6)
+        val rollResult = dice.roll(6)
+        assertTrue("The value of rollResult was not between 1 and 6", rollResult in 1..6)
     }
 }
